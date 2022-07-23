8a971e21e6c149d49524669833bdd0b6b5cda1ba
Ethan Julius <56324629+500EJ@users.noreply.github.com>
2022-07-23 12:20:22 -0400

    Add specs for reverseString and implement function

--- problems/reverse-string.js
+++ problems/reverse-string.js
@@ -1,3 +1,4 @@
 module.exports = function reverseString(string) {
-  // Your code here
+  if (typeof string !== "string") throw TypeError("Expected a string");
+  return string.split("").reverse().join("");
 };
--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,12 @@
-// Your code here
+const { expect } = require("chai");
+const reverseString = require("../problems/reverse-string");
+
+describe("reverseString(string)", () => {
+  it("should return the passed in string reversed", () => {
+    expect(reverseString("fun")).to.equal("nuf");
+  });
+
+  it("should throw a TypeError if a string is not passed in", () => {
+    expect(() => reverseString(42)).to.throw(TypeError, "Expected a string");
+  });
+});
